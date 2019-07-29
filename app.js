eab793b35d400bded52271c5b43bcc19d29ebbbf
Adam <spincycle01@yahoo.com>
2019-07-29 12:28:08 -0400

    JQuery set up for API request, event listener added to button

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // 'http://aws.random.cat//meow'
+
+const img = $('#catImg');
+const styles = {
+  backgroundColor: function() {
+    return `rgb(${Math.random() * 256}, ${Math.random() *
+      256}, ${Math.random() * 256})`;
+  },
+  color: function() {
+    return `rgb(${Math.random() * 256}, ${Math.random() *
+      256}, ${Math.random() * 256})`;
+  },
+  outline: 'none'
+};
+
+$('#btn').on('click', function() {
+  $.getJSON('http://aws.random.cat//meow')
+    .then(data => {
+      img.attr('src', data['file']);
+      $(this).css(styles);
+    })
+    .fail(error => console.log('error is:', error));
+});
